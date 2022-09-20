29d2d20297db2b0fcb4d5bfe1eee82d9fefcfb7e
Tejas28 <pathak.tejas28@gmail.com>
2022-09-20 17:36:42 +0530

    Sonarqube plugin check

--- src/main/java/com/devsecops/NumericController.java
+++ src/main/java/com/devsecops/NumericController.java
@@ -7,6 +7,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
+//just want to commit
 
 @RestController
 public class NumericController {
